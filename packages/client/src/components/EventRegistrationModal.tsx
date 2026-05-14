acefb3d6a103946afae4e840c20740155f39b583
gemFromJa <stephankw16@gmail.com>
2026-05-14 15:56:18 -0500

    Minor event->login url bug fix

--- packages/client/src/components/EventRegistrationModal.tsx
+++ packages/client/src/components/EventRegistrationModal.tsx
@@ -75,7 +75,7 @@ export default function EventModal({ event, onClose }: Props) {
 
   function handleJoin() {
     if (!user) {
-      navigate("/auth");
+      navigate("/auth/login");
       onClose();
       return;
     }
